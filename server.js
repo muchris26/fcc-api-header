d2397a0c607c4df9a2b963ed2f3c95958e1b7987
muchris26 <cderesz@gmail.com>
2017-01-11 15:47:52 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	var parsedURL = url.parse(request.url);
+	var URLpath = parsedURL.pathname.replace("/", "");
+	response.write(date_test(URLpath));
+	response.end();	
+}).listen(8888);
+
+function date_test(date_in) {
+	var date1 = parseInt(date_in);
+	console.log(date_in.replace(/[A-Za-z]/gi, ""));
+	if (date_in.replace(/[A-Za-z]/gi, "").length < 13) {
+		date1 = Date.parse(date_in.replace("%", " "));
+	}
+	var date2 = new Date(date1).toDateString();
+	console.log(date_in, date_in.replace(/%/gi, " "), date1, date2);
+	return '{"unix": ' + date1 + ', "natural": ' + date2 + '}';
+}
+
+// localhost:8888/Dec 11 2011
+// Formats accepted:
+// Month/YYYY/D
+// Month-YYYY-D
+// M/D/YYYY
+// M-D-YYYY
